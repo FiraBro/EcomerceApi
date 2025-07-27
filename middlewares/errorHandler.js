cc6a3bd9d48b0bd1ce4047d8c2b1c388f3dbd79f
Firagos <jemalfiragos@gmail.com>
2025-07-27 17:53:20 +0300

    making to fix the problem of try catch block

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,9 +1,24 @@
-const errorHandler = (err, req, res, next) => {
-  const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
-  res.status(statusCode).json({
-    message: err.message,
-    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+// middleware/errorHandler.js
+import { logger } from "../utils/logger.js";
+import { AppError } from "../utils/AppError.js";
+
+export const errorHandler = (err, req, res, next) => {
+  logger.error(err);
+
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+
+  if (err instanceof AppError) {
+    return res.status(statusCode).json({
+      success: false,
+      status: err.status,
+      message: err.message,
+    });
+  }
+
+  res.status(500).json({
+    success: false,
+    status: "error",
+    message: "Something went very wrong!",
   });
 };
-
-export default errorHandler;
