2eb88d3f9a4532f88375b28d4c64d129a77f41fb
Firagos <jemalfiragos@gmail.com>
2025-07-28 18:05:18 +0300

    creating product route

---
+++ routes/productRoutes.js
@@ -0,0 +1,10 @@
+// routes/productRoutes.js
+import express from "express";
+import upload from "../middlewares/upload.js";
+import { createProduct } from "../controllers/productController.js";
+
+const productRoute = express.Router();
+
+productRoute.post("/", upload.single("image"), createProduct);
+
+export default productRoute;
