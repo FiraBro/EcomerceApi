cba88612bbe9e670bf1ec6fac42553a9a724d1d6
Firagos <jemalfiragos@gmail.com>
2025-07-28 18:04:39 +0300

    creating category route

---
+++ routes/categoryRoutes.js
@@ -0,0 +1,9 @@
+// routes/categoryRoutes.js
+import express from "express";
+import { createCategory } from "../controllers/categoryController.js";
+
+const categoryRoute = express.Router();
+
+categoryRoute.post("/", createCategory);
+
+export default categoryRoute;
