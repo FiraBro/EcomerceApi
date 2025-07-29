38a00718e88849000bc8aa1748e8b366f38384aa
Firagos <jemalfiragos@gmail.com>
2025-07-29 10:31:12 +0300

    adding the search function to it

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,9 +1,13 @@
 // routes/productRoutes.js
 import express from "express";
 import upload from "../middlewares/upload.js";
-import { createProduct } from "../controllers/productController.js";
+import {
+  createProduct,
+  searchProducts,
+} from "../controllers/productController.js";
 
 const productRoute = express.Router();
 
 productRoute.post("/", upload.single("image"), createProduct);
+productRoute.get("/search", searchProducts);
 export default productRoute;
