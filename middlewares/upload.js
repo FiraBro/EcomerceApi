9cd4bf42c5b58f533476efea9bdfcad0623d4aad
Firagos <jemalfiragos@gmail.com>
2025-07-29 10:15:06 +0300

    setuping multer

---
+++ middlewares/upload.js
@@ -0,0 +1,24 @@
+// middlewares/upload.js
+import multer from "multer";
+import path from "path";
+import { fileURLToPath } from "url";
+import fs from "fs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const dest = path.join(__dirname, "../uploads/products");
+    fs.mkdirSync(dest, { recursive: true }); // ensure folder exists
+    cb(null, dest);
+  },
+  filename: (req, file, cb) => {
+    const uniqueName = Date.now() + path.extname(file.originalname);
+    cb(null, uniqueName);
+  },
+});
+
+const upload = multer({ storage });
+
+export default upload;
