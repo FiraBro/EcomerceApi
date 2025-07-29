22c034b075d7d2f24dde68ad9b2152fc8db835d3
Firagos <jemalfiragos@gmail.com>
2025-07-29 10:18:03 +0300

    adding favorite route

---
+++ routes/favoriteRoutes.js
@@ -0,0 +1,17 @@
+import express from "express";
+import {
+  addFavorite,
+  getFavorites,
+  removeFavorite,
+} from "../controllers/favoriteController.js";
+import { protect } from "../middleware/authMiddleware.js";
+
+const favoriteRoute = express.Router();
+
+favoriteRoute.use(protect);
+
+favoriteRoute.route("/").get(getFavorites).post(addFavorite);
+
+favoriteRoute.route("/:itemId").delete(removeFavorite);
+
+export default favoriteRoute;
