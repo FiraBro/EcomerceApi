77c5bdde3d72ba1da23ff1f52d909a122364abe0
Firagos <jemalfiragos@gmail.com>
2025-07-29 10:19:26 +0300

    simple modification

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,9 +1,11 @@
 // routes/categoryRoutes.js
 import express from "express";
 import { createCategory } from "../controllers/categoryController.js";
+import { getProductsByCategory } from "../controllers/productController.js";
 
 const categoryRoute = express.Router();
 
 categoryRoute.post("/", createCategory);
+categoryRoute.get("/:categoryId/products", getProductsByCategory);
 
 export default categoryRoute;
