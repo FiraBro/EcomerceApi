6bcedc8c83e3f7c922223bc329c3aec3b6e509fb
Firagos <jemalfiragos@gmail.com>
2025-07-30 17:11:43 +0300

    adding protect and admin middleware

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,9 +5,16 @@ import {
   createProduct,
   searchProducts,
 } from "../controllers/productController.js";
+import { protect, admin } from "../middlewares/authMiddleware.js";
 
 const productRoute = express.Router();
 
-productRoute.post("/", upload.single("image"), createProduct);
+productRoute.post(
+  "/add",
+  protect,
+  admin,
+  upload.single("image"),
+  createProduct
+);
 productRoute.get("/search", searchProducts);
 export default productRoute;
