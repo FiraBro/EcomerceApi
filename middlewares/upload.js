17d49abf0a82fac0f9cb5d0979dfaee536ae3644
Firagos <jemalfiragos@gmail.com>
2025-07-31 17:26:20 +0300

    mmaking more dynamic

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -7,18 +7,20 @@ import fs from "fs";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const dest = path.join(__dirname, "../uploads/products");
-    fs.mkdirSync(dest, { recursive: true }); // ensure folder exists
-    cb(null, dest);
-  },
-  filename: (req, file, cb) => {
-    const uniqueName = Date.now() + path.extname(file.originalname);
-    cb(null, uniqueName);
-  },
+const createStorage = (folderName) =>
+  multer.diskStorage({
+    destination: (req, file, cb) => {
+      const dest = path.join(__dirname, `../uploads/${folderName}`);
+      fs.mkdirSync(dest, { recursive: true }); // ensure folder exists
+      cb(null, dest);
+    },
+    filename: (req, file, cb) => {
+      const uniqueName = Date.now() + path.extname(file.originalname);
+      cb(null, uniqueName);
+    },
+  });
+
+export const uploadProductImage = multer({
+  storage: createStorage("products"),
 });
-
-const upload = multer({ storage });
-
-export default upload;
+export const uploadHeroImage = multer({ storage: createStorage("hero") });
