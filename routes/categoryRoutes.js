5a7540a197fad3cc55b19adc36863c3d457db5ac
Firagos <jemalfiragos@gmail.com>
2025-07-31 17:24:19 +0300

    adding authMiddlewar to it

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -2,10 +2,11 @@
 import express from "express";
 import { createCategory } from "../controllers/categoryController.js";
 import { getProductsByCategory } from "../controllers/productController.js";
+import { admin, protect } from "../middlewares/authMiddleware.js";
 
 const categoryRoute = express.Router();
 
-categoryRoute.post("/", createCategory);
-categoryRoute.get("/:categoryId/products", getProductsByCategory);
+categoryRoute.post("/", protect, admin, createCategory);
+categoryRoute.get("/:categoryId/products", protect, getProductsByCategory);
 
 export default categoryRoute;
