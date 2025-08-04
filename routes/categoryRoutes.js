6d30afa85eee00f55cda9f1e35f5cf171ced98bf
Firagos <jemalfiragos@gmail.com>
2025-08-04 17:02:18 +0300

    adding a route that get all category

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,12 +1,16 @@
 // routes/categoryRoutes.js
 import express from "express";
-import { createCategory } from "../controllers/categoryController.js";
+import {
+  createCategory,
+  getAllCategories,
+} from "../controllers/categoryController.js";
 import { getProductsByCategory } from "../controllers/productController.js";
 import { admin, protect } from "../middlewares/authMiddleware.js";
 
 const categoryRoute = express.Router();
 
 categoryRoute.post("/", protect, admin, createCategory);
+categoryRoute.get("/getAllCategory", protect, getAllCategories);
 categoryRoute.get("/:categoryId/products", protect, getProductsByCategory);
 
 export default categoryRoute;
